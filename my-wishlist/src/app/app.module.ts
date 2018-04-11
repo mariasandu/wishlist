9bf40f92d3e954b1bef1a2a4db8b81a932450727
Maria <msandu@macadamian.com>
2018-04-11 17:35:56 +0300

    AngularFire - first step was to create a Firebase project in the Firebase console - install angularfire2 - initialize Firebase by adding the initialization code snippet

--- my-wishlist/src/app/app.module.ts
+++ my-wishlist/src/app/app.module.ts
@@ -2,9 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBRUuuOYHR1U3UWztZvlRSe2vEnLL_GBX4",
+  authDomain: "my-wishlist-datastore.firebaseapp.com",
+  databaseURL: "https://my-wishlist-datastore.firebaseio.com",
+  projectId: "my-wishlist-datastore",
+  storageBucket: "my-wishlist-datastore.appspot.com",
+  messagingSenderId: "799712116794"
+};
 
 
 @NgModule({
@@ -14,7 +24,8 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
